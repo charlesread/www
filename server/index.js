403c4dac7317c95b3cb425058a8c3898fbb38580
Charles Read <charles@charlesread.io>
2023-04-22 18:17:53 -0400

    inc

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-import * as path from "path"
-import * as url from 'url';
+import * as path from 'path'
+import * as url from 'url'
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 import fastifyFactory from 'fastify'
 import fastifyStatic from '@fastify/static'
 
 const fastify = fastifyFactory()
-
 
 fastify.register(fastifyStatic, {
     root: path.resolve(__dirname, '..', 'dist'),
@@ -16,15 +15,17 @@ fastify.register(fastifyStatic, {
 })
 
 const start = async () => {
-    try {
-        await fastify.listen({host: '0.0.0.0', port: 8080})
-    } catch (err) {
-        console.error(err)
-        process.exit(1)
-    }
+    return await fastify.listen({host: '0.0.0.0', port: 8080})
 }
 start()
-    .then(function () {
-        console.log('listening')
+    .then(function (address) {
+        console.log(`listening at ${address}`)
+        process.on('SIGINT', function (e) {
+            console.log(`caught ${e}`)
+            process.exit(0)
+        })
     })
-    .catch(console.error)
+    .catch(function (err) {
+        console.error(err)
+        process.exit(1)
+    })
